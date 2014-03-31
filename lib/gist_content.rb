03330a62b209cea8089160816323ec5dcb02f53a
katmutua <jkm.mutua@gmail.com>
2014-03-31 17:54:07 -0300

    Add modules

--- lib/gist_content.rb
+++ lib/gist_content.rb
@@ -2,19 +2,24 @@ require_relative '../lib/gist_file'
 require_relative '../lib/gist_setup'
 require 'open-uri'
 
-class GistContent
+module CheatSheet
+  class GistContent
 
-  def gist_file
-    GistFile.new
-  end
+    def gist_file
+      CheatSheet::GistFile.new
+    end
 
-  def fetch_content
-    meta = GistSetup.new
-    open(meta[:url]).read
-  end
+    def fetch_content
+      meta = CheatSheet::GistSetup.new.gist_meta
+      open(meta[:url]).read
+    end
 
-  def write_to_file
-    gist_file.write_to_file fetch_content
+    def write_to_file
+      gist_file.write_to_file fetch_content
+    end
   end
 
 end
+
+#content  = CheatSheet::GistContent.new
+#content.write_to_file
--- lib/gist_file.rb
+++ lib/gist_file.rb
@@ -1,31 +1,35 @@
-class GistFile
+require_relative '../lib/gist_setup'
 
-  attr_accessor :file_name, :file_type
+module CheatSheet
+  class GistFile
 
-  def initialize
-    meta = GistSetup.new
-    @file_name = meta[:file_name]
-    @file_type = meta[:file_type]
-  end
+    attr_accessor :file_name, :file_type
 
-  def read_file
-    File.read(gist_file)
-  end
+    def initialize
+      meta = CheatSheet::GistSetup.new.gist_meta
+      @file_name = meta[:file_name]
+      @file_type = meta[:file_type]
+    end
 
-  def path_to_file
-    File.expand_path("./#{@file_name}.#{file_type}")
-  end
+    def read_file
+      File.read(gist_file)
+    end
 
-  def destroy_file
-    File.delete(path_to_file)
-  end
+    def path_to_file
+      File.expand_path("./#{@file_name}.#{file_type}")
+    end
 
-  def write_to_file(content)
-    create_file.write(content)
-  end
+    def destroy_file
+      File.delete(path_to_file)
+    end
 
-  def create_file
-    File.new(@file_name+".#{@file_type}", "w") unless File.file?(@file_name)
+    def write_to_file(content)
+      create_file.write(content)
+    end
+
+    def create_file
+      File.new(@file_name+'.txt', 'w') unless File.file?(@file_name)
+    end
   end
 
 end
