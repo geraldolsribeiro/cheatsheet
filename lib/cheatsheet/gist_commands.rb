989c0598806480d81702808fdbf79bad2bec2ddf
katmutua <jkm.mutua@gmail.com>
2014-04-02 14:18:37 -0300

    Use a simpler option parser in gist_commands

---
+++ lib/cheatsheet/gist_commands.rb
@@ -0,0 +1,59 @@
+require 'ostruct'
+require 'date'
+
+module CheatSheet
+  class GistCommands
+
+    def initialize(arguments, stdin)
+      @arguments = arguments
+      @stdin = stdin
+
+      @options = OpenStruct.new
+      @options.verbose = false
+      @options.quiet = false
+    end
+
+    def run
+      output_options
+    end
+
+    protected
+
+    def process_options
+      @options.verbose = false if @options.quiet
+    end
+
+    def parsed_options?
+      opts = OptionParser.new (ARGV)
+      opts.on('-v', '--version') { output_version; exit 0 }
+      opts.on('-h', '--help') { output_help }
+      opts.on('-V', '--verbose') { @options.verbose = true }
+      opts.on('-q', '--quiet') { @options.quiet = true }
+      opts.parse!(@arguments) rescue return false
+
+      process_options
+      true
+    end
+
+    def arguments_valid?
+      true if @arguments.length == 1
+    end
+
+    def process_arguments
+      # TO DO - place in local vars, etc
+    end
+
+    def process_command
+      # TO DO - do whatever this app does
+    end
+
+    def output_options
+      puts "Options:\
+           "
+
+      @options.marshal_dump.each do |name, val|
+        puts "  #{name} = #{val}"
+      end
+    end
+  end
+end
