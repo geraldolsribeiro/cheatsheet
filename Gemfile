c29a19f9cdfc5cc40b8ee25cdabaf4f893672893
katmutua <jkm.mutua@gmail.com>
2014-04-03 08:43:49 -0300

    Add some formatting for commands on terminal

--- Gemfile
+++ Gemfile
@@ -4,9 +4,7 @@ ruby "2.0.0"
 
 gem 'rake'
 gem 'slop'
-Ggem 'eventmachine'
 gem 'activesupport'
-
 
 group :test do
   gem 'rspec'
--- lib/cheatsheet/gist_commands.rb
+++ lib/cheatsheet/gist_commands.rb
@@ -3,26 +3,65 @@ require 'date'
 
 module CheatSheet
   class GistCommands
+    attr_accessor :options
+
+    def initialize
+      puts "Press ':help' for help with cheatsheet commands".c (94)
+      @options = {
+          :links => "Working with links",
+          :buttons => "Interacting with buttons",
+          :navigating => "Moving between pages",
+          :interactions => "Interacting with forms",
+          :scoping => "Page interaction and scoping",
+          :scripting => "Carrying out scripting",
+          :debugging => "Lists all commands",
+          :javascript => "Working with asynchronous Javascript",
+          :selectors => "Css and xpath selectors for elements"}
+    end
 
     def process_command
       while input = gets
         if input.chomp == ':exit'
           exit
         end
-        process_options(input)
+
+        if input.chomp == ':help'
+          output_options
+        end
+
       end
     end
 
-    def process_options(option)
-
+    def output_options
+      @options.each do |option|
+        option_key = option.first
+        value = @options[option_key].to_s
+        puts "#{option_key.to_s.indent(4).c(96)} #{value.indent(4).c(92)}"
+      end
     end
   end
+end
 
-  def output_options
-    puts "Options:\
-         "
-    @options.marshal_dump.each do |name, val|
-      puts "  #{name} = #{val}"
-    end
+class String
+  def c(*codes)
+    codes = codes.flatten.map { |code|
+      case code
+        when String, Symbol
+          CheatSheet.config[:color][code.to_sym] rescue nil
+        else
+          code
+      end
+    }.compact.unshift(0)
+    "\e[#{codes.join(';')}m#{self}\e[0m"
+  end
+
+  def indent(count, char = ' ')
+    (char * count) + gsub(/(\n+)/) { |m| m + (char * count) }
+  end
+
+  def trim_indent
+    lines = self.split("\n")
+    unindent = self.split("\n").select { |s| s !~ /^\s$/ }.map { |s| s.index(/[^\s]/) || 0 }.min
+    lines.map { |s| s.gsub(/^#{' ' * unindent}/, '') }.join("\n")
   end
 end
